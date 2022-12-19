dea22cc5deadd14cb31e50d587fc753ababe3f45
jamie <jl10135@nyu.edu>
2022-12-19 17:38:37 -0500

    additional visualizer + playhead fixes

--- playhead.js
+++ playhead.js
@@ -13,11 +13,11 @@ let totalDuration = document.querySelector(".totalDuration");
 let trackIndex = 0;
 let isPlaying = false;
 let updateTimer;
-
+let loading = false;
 // audio element
 let currentTrack = document.createElement('audio');
 
-// create tracklist
+// create tracklist array with all necessary info
 let trackList = [
     {
         name: "starting line",
@@ -81,102 +81,84 @@ let trackList = [
     },
 ];
 
+trackName.textContent = trackList[trackIndex].name;
 
-function loadTrack(trackIndex) {
-    clearInterval (updateTimer);
-    resetValues();
-
-    // load track
-    currentTrack.src = trackList[trackIndex].path;
-    currentTrack.load();
-
-    // update track name
-    trackName.textContent = trackList[trackIndex].name;
-
-    // update seek slider
-    updateTimer = setInterval(seekUpdate, 1000);
-
-    // track rotation
-    currentTrack.addEventListener("ended", nextTrack)
-
-    song.stop()
-    songReload()
-    song.play()
-}
-
-// resetting slider + time values
-function resetValues() {
-    currentTime.textContent = "00:00";
-    totalDuration.textContent = "00:00";
-    seekSlider.value = 0;
-}
-
-function playpauseTrack() {
-    if (!isPlaying) playTrack();
-    else pauseTrack();
-}
-
+// buttons with html elements and positioning/status
 function playTrack() {
-    currentTrack.play();
-    isPlaying = true;
-
+    setInterval(seekUpdate, 1000);
     playpauseButton.innerHTML = '<i class = "fa fa-pause-circle fa-4x"></i>';
 }
 
 function pauseTrack() {
-    currentTrack.pause();
-    isPlaying = false;
-
     playpauseButton.innerHTML = '<i class = "fa fa-play-circle fa-4x"></i>';
 }
 
 function nextTrack() {
-    if (trackIndex < trackList.length - 1)
-        trackIndex += 1;
-    else trackIndex = 0;
-    loadTrack(trackIndex);
-    playTrack();
+    pauseTrack();
+    song.stop();
+    loading = true;
+    trackIndex += 1;
+    // maintaining proper tracklist positioning
+    if(trackIndex == trackList.length){
+        trackIndex = 0;
+    }
+    // load song given that "loading" is false, otherwise sketch will crash in between audios
+    song = loadSound(trackList[trackIndex].path,function (){loading = false;
+        trackName.textContent = trackList[trackIndex].name;
+        
+    }); 
+    trackName.textContent = "loading...";
 }
 
 function prevTrack() {
-    if (trackIndex > 0)
-        trackIndex -= 1;
-    else trackIndex = trackList.length;
-    loadTrack(trackIndex);
-    playTrack();
+    pauseTrack();
+    song.stop();
+    loading = true;
+    trackIndex -= 1;
+    if(trackIndex < 0){
+        trackIndex = trackList.length - 1;
+    }
+        trackName.textContent = trackList[trackIndex].name;
+    song = loadSound(trackList[trackIndex].path,function (){loading = false;
+        trackName.textContent = trackList[trackIndex].name;
+    });
+    trackName.textContent = "loading...";
 }
 
 function seekTo() {
-    seekTo = currentTrack.duration * (seekSlider.value / 100);
-    currentTrack.currentTime = seekTo;
+    console.log(seekSlider.value);
+    song.jump(map(seekSlider.value,1,100,0,song.duration()));
 }
 
-function setVolume() {
-    currentTrack.volume = volumeSlider.value / 100;
+function setVol() {
+    console.log(volumeSlider.value);
+    song.setVolume(map(volumeSlider.value,0,100,0,1));
 }
 
 function seekUpdate() {
-    let seekPosition = 0;
+    let seekPosition = map(song.currentTime(),0,song.duration(),1,100);
+    if(seekPosition >= 99){
+       seekPosition = 0;
+       nextTrack();
+    }
 
-    if (!isNaN(currentTrack.duration)) {
-        seekPosition = currentTrack.currentTime * (100 / currentTrack.duration);
         seekSlider.value = seekPosition;
 
         // calculate time left & duration
-        let currentMinutes = Math.floor(currentTrack.currentTime / 60);
-        let currentSeconds = Math.floor(currentTrack.currentTime - currentMinutes * 60);
-        let durationMinutes = Math.floor(currentTrack.duration / 60);
-        let durationSeconds = Math.floor(currentTrack.duration - durationMinutes * 60);
+        let currentMinutes = floor(song.currentTime()/60);
+        let currentSeconds = floor(song.currentTime()-currentMinutes*60);
+        let durationMinutes = floor(song.duration() / 60);
+        let durationSeconds = floor(song.duration() - durationMinutes * 60);
         // adding zeroes so it's consistent
         if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
         if (durationSeconds < 10) {durationSeconds = "0" + durationSeconds; }
         if (currentMinutes < 10) {currentMinutes = "0" + currentMinutes; }
         if (durationMinutes < 10) {durationMinutes = "0" + durationMinutes; }
-    }
+
+    currentTime.textContent = currentMinutes+":"+currentSeconds;
+    totalDuration.textContent = durationMinutes+":"+durationSeconds;
 }
 
-// load track
-loadTrack(trackIndex)
 
 
 
--- sketch.js
+++ sketch.js
@@ -1,25 +1,25 @@
-//INITIAL FRAMEWORK
-// followed tutorial https://www.youtube.com/watch?v=uk96O7N1Yo0&t=901s
+// followed tutorial/foundation of https://www.youtube.com/watch?v=uk96O7N1Yo0&t=901s
 
-var song
-var img
-var fft
-var particles = []
-var firstTime = true
+let song;
+let img;
+let fft;
+let particles = [];
+let firstTime = true;
 
-function preload(){
-  img = loadImage('album-bg.jpeg')
-  song = loadSound(trackList[trackIndex].path)
-  song.setVolume(0.6)
+function preload() {
+  img = loadImage('album-bg.jpeg');
+  song = loadSound(trackList[trackIndex].path);
+  song.setVolume(0.5)
 }
 
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight)
-  canvas.position(0,0)
-  canvas.style('z-index', '-1')
-  angleMode(DEGREES)
-  imageMode(CENTER)
-  rectMode(CENTER)
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0, 0);
+  // layer order
+  canvas.style('z-index', '-1');
+  angleMode(DEGREES);
+  imageMode(CENTER);
+  rectMode(CENTER);
 
   // fft variable is 0.8 by default, the higher it is the smoother it is from one value to another
   //by lowering it to 0.3 the image movement is more prominent
@@ -53,7 +53,7 @@ function draw() {
   pop()
 
   // black rectangle overlay
-  var alpha = map(amp, 0, 255, 180, 150)
+  let alpha = map(amp, 0, 255, 180, 150)
   fill(0, alpha)
   noStroke()
   rect(0, 0, width, height)
@@ -63,73 +63,115 @@ function draw() {
   noFill()
 
   // actual waveform from fft (fast fourier transform)
-  var wave = fft.waveform()
+  let wave = fft.waveform()
 
- push()
- translate(0, -120)
-// creates two semicircles
- for (var t = -1; t <= 1; t += 2) {
-   beginShape()
-   for (var i = 0; i <= 180; i += 0.5) {
-     var index = floor(map(i, 0, 180, 0, wave.length - 1))
-     var avgRadius = 180
-     var sharpness = 25
-     // 20 was originally 150 (large circle in center)
-     var radius = map(wave[index], -1, 1, avgRadius - sharpness, avgRadius + sharpness)
-     var x = radius * sin(i) * t
-     var y = radius * cos(i)
-     vertex(x, y)
-  }
-   endShape()
-  
-}
+  push();
+  translate(0, -120);
+  if (lineVisualizer) {
+    for (let i = 0; i <= 80; i += 1) {
+      let index = floor(map(i, 0, 80, 0, wave.length - 1));
+      let sharpness = map(volumeSlider.value, 0, 100, 0, height / 6);
+      let x = map(i, 0, 80, -width / 2, width / 2);
+      let y = map(wave[index], -1, 1, -sharpness * 3, sharpness * 3);
+      line(x, -y, x, y);
 
-for (var t = -1; t <= 1; t += 2) {
-  beginShape()
-  for (var i = 0; i <= 180; i += 0.5) {
-    var index = floor(map(i, 0, 180, 0, wave.length - 1))
-    var avgRadius = 50
-    var sharpness = 5
-    // 20 was originally 150 (large circle in center)
-    var radius = map(wave[index], -1, 1, avgRadius - sharpness, avgRadius + sharpness)
-    var x = radius * sin(i) * t
-    var y = radius * cos(i)
-    vertex(x, y)
- }
-  endShape()
- 
-}
-
-// particles & reacting to the beat
-var p = new Particle()
-particles.push(p)
-
-// decides which particles to show depending on position
-for (var i = particles.length - 1; i >= 0; i--) {
-  if (!particles[i].edges()) {
-    particles[i].update(amp > 230)
-    particles[i].show()
+    }
   } else {
-    particles.splice(i, 1)
+    //  }
+    // creates two semicircles
+    for (let t = -1; t <= 1; t += 2) {
+      console.log(volumeSlider.value);
+      beginShape();
+      for (let i = 0; i <= 180; i += 0.5) {
+        let index = floor(map(i, 0, 180, 0, wave.length - 1))
+        let avgRadius = 180;
+        let sharpness = map(volumeSlider.value, 0, 100, 0, height / 10);
+        // 20 was originally 150 (large circle in center)
+        let radius = map(wave[index], -1, 1, avgRadius - sharpness, avgRadius + sharpness);
+        let x = radius * sin(i) * t;
+        let y = radius * cos(i);
+        vertex(x, y);
+      }
+      endShape();
+
+    }
+
+    for (let t = -1; t <= 1; t += 2) {
+      beginShape();
+      for (let i = 0; i <= 180; i += 0.5) {
+        let index = floor(map(i, 0, 180, 0, wave.length - 1));
+        let avgRadius = 50;
+        let sharpness = map(volumeSlider.value, 0, 100, 0, height / 50);
+        // 20 was originally 150 (large circle in center)
+        let radius = map(wave[index], -1, 1, avgRadius - sharpness, avgRadius + sharpness);
+        let x = radius * sin(i) * t;
+        let y = radius * cos(i);
+        vertex(x, y)
+      }
+      endShape()
+
+    }
   }
-}
-pop()
 
+  // particles & reacting to the beat
+
+  // only have active particles with circle visualizer
+  if (lineVisualizer == false) {
+    let p = new Particle();
+    particles.push(p);
+
+    // decides which particles to show depending on position
+    for (let i = particles.length - 1; i >= 0; i--) {
+      if (!particles[i].edges()) {
+        particles[i].update(amp > 230)
+        particles[i].show()
+      } else {
+        particles.splice(i, 1)
+      }
+    }
+  }
+  pop();
+
+  //line visualizer switch (bottom right corner)
+  fill(150);
+  noStroke();
+  rect(width / 2 - 30, height / 2 - 30, 30, 15, 15);
+  fill(60);
+  if (lineVisualizer) {
+    circle((width / 2 - 30) + 15 / 2, (height / 2 - 30), 15);
+    //rect(0,0,100);
+  }
+  else {
+    circle((width / 2 - 30) - 15 / 2, (height / 2 - 30), 15);
+  }
+  noFill();
+  fill(200);
 }
 
-function songReload() {
-  song = loadSound(trackList[trackIndex].path)
-  song.setVolume(0.6)
-}
+// reset
+let lineVisualizer = false;
 
 // audio plays/stops on mouseclick as browsers don't support autoplay
 function mouseClicked() {
-  if (song.isPlaying()) {
-    song.pause()
-    noLoop()
-  } else {
-    song.play()
-    loop()
+  if (dist(0 + width / 2, height / 3.4 + height / 2, mouseX, mouseY) < 65 && loading == false) {
+    if (song.isPlaying()) {
+      song.pause();
+      pauseTrack();
+      noLoop()
+    } else {
+      playTrack();
+      song.play();
+      loop()
+    }
+  }
+
+  // since translate doesn't affect mouseX and mouseY
+  let x = width - 30;
+  let y = height - 30;
+  // console.log(dist(mouseX,mouseY,x,y));
+  // detect if user pressed on shape change button
+  if (dist(mouseX, mouseY, x, y) < 30) {
+    lineVisualizer = !lineVisualizer;
   }
 }
 
@@ -138,37 +180,41 @@ class Particle {
   constructor() {
     // orig multi by 250 for large circle in center
     // positions particles randomly on radius of circle
-    this.pos = p5.Vector.random2D().mult(180)
+    this.pos = p5.Vector.random2D().mult(180);
     // particle movement
-    this.vel = createVector(0, 0)
-    this.acc = this.pos.copy().mult(random(0.0001, 0.00001))
+    this.vel = createVector(0, 0);
+    this.acc = this.pos.copy().mult(random(0.0001, 0.00001));
 
-    this.width = random(3, 5)
+    this.width = random(3, 5);
 
-    this.color = [random (200, 255), random(200, 255), random(200, 255),]
+    this.color = [random(200, 255), random(200, 255), random(200, 255),];
   }
   // beat detection; if cond is true particles move faster
   update(cond) {
-    this.vel.add(this.acc)
-    this.pos.add(this.vel)
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
     if (cond) {
-      this.pos.add(this.vel)
-      this.pos.add(this.vel)
-      this.pos.add(this.vel)
+      this.pos.add(this.vel);
+      this.pos.add(this.vel);
+      this.pos.add(this.vel);
+      this.pos.add(this.vel);
     }
   }
+
+  // limits where particles go (canvas boundaries)
   edges() {
     if (this.pos.x < -width / 2 || this.pos.x > width / 2 ||
       this.pos.y < - height / 2 || this.pos.y > height / 2) {
-        return true
-      } else {
-        return false
-      }
+      return true;
+    } else {
+      return false;
+    }
   }
+
   show() {
-    noStroke()
-    fill(this.color)
-    ellipse(this.pos.x, this.pos.y, this.width)
+    noStroke();
+    fill(this.color);
+    ellipse(this.pos.x, this.pos.y, this.width);
   }
 }
 
